e6dafb21eeb32bcbeb8a650a4c2c246088d6da83
Mateo Tamayo <christianmateotamayo@gmail.com>
2024-12-18 13:46:36 +0100

    Trabajo con callbacks

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,6 +1,16 @@
 //desestructuración 
 
-const {basicTemplate} = require('./js-foundation/01-tempate');
+// const {basicTemplate} = require('./js-foundation/01-tempate');
 
-require('./js-foundation/02-destructuring')
+// require('./js-foundation/02-destructuring')
 
+const { getUserById } =require('./js-foundation/03-callbacks');
+
+const id = 3;
+
+getUserById(id, function(error, user) {
+    if(error) {
+        throw new Error(error);
+    }
+    console.log(user);
+});
