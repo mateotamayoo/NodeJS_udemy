af4b135b497cd21898227f779fb59ccd920107ff
Mateo Tamayo <christianmateotamayo@gmail.com>
2024-12-31 12:02:18 +0100

    feat(test): add jest to test and create test to app.ts

--- 02-bases/src/app.ts
+++ 02-bases/src/app.ts
@@ -27,15 +27,15 @@
 
 
 
-import { buildLogger } from "./plugins/logger.plugin";
+// import { buildLogger } from "./plugins/logger.plugin";
 
-const logger = buildLogger('app.js');
+// const logger = buildLogger('app.js');
 
-logger.log('Hello from app.js');
+// logger.log('Hello from app.js');
 
 
-export const name:string = "fernado";
-console.log(name)
+// export const name:string = "fernado";
+// console.log(name)
 
 
 
