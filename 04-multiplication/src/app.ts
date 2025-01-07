ffd56d15a6b47fd55a7b7479492447a16f676083
Mateo Tamayo <christianmateotamayo@gmail.com>
2025-01-07 10:38:59 +0100

    feat(yargs): add yargs to prod dependecies and anonymous functions

---
+++ 04-multiplication/src/app.ts
@@ -0,0 +1,13 @@
+import { yarg } from './config/plugins/args.plugin';
+
+//console.log(yarg); 
+
+
+(async() =>  {
+    await main();
+})();
+
+async function main() {
+    console.log('Hello, World!');
+    
+}
---
+++ 04-multiplication/src/config/plugins/args.plugin.ts
@@ -0,0 +1,6 @@
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+export const yarg = yargs(hideBin(process.argv))
+    .parseSync();
