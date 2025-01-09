d074a20d99aeb4f67bae17985c4a2a5699c23b2a
Mateo Tamayo <christianmateotamayo@gmail.com>
2025-01-09 13:00:43 +0100

    feat(server-app): implementing server-app test

--- 04-multiplication/src/presentation/server-app.test.ts
+++ 04-multiplication/src/presentation/server-app.test.ts
@@ -1,9 +1,51 @@
 import { ServerApp } from './server-app';
+import { CreateTable } from '../domain/use-cases/create-table.use-case';
+import { SaveFile } from '../domain/use-cases/save-file.use-case';
+
 
 describe('Server App', () => {
+
+    const options = {
+        base: 2,
+        limit:10,
+        showTable: true,
+        destination: 'test-destination',
+        name: 'test-filename'
+    };
+
     test('should create serverapp instance', () => {
         const serverapp = new ServerApp();
         expect (serverapp).toBeInstanceOf(ServerApp);
-        expect (ServerApp.run).toBe('function');
+        expect (typeof ServerApp.run).toBe('function');
     });
+
+    test('should run Serverapp with options', () => {
+
+        // const logSpy = jest.spyOn(console,'log');
+        // const createTablespy = jest.spyOn(CreateTable.prototype, 'execute');
+        // const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+
+
+
+        // ServerApp.run(options);
+
+        // expect(logSpy).toHaveBeenCalledTimes(3);
+
+        // expect(createTablespy).toHaveBeenCalledWith({
+        //     base:options.base, limit: options.limit
+        // })
+
+        // expect(saveFileSpy).toHaveBeenCalledWith({
+        //     fileContent: expect.any(String),
+        //     fileDestination: options.destination,
+        //     fileName: options.name
+        // })
+
+        test('should run with custom values mocked', () => {
+
+            
+            ServerApp.run(options);
+        })
+    })
+
 });
